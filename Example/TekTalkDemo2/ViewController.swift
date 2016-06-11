55b7db65ce6c27b0d96e29920f7af17d1c37bb20
Phan An <quoc.an@whizzy.co.jp>
2016-06-11 19:21:44 +0700

    Initial commit

---
+++ Example/TekTalkDemo2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TekTalkDemo2
+//
+//  Created by Phan An on 06/11/2016.
+//  Copyright (c) 2016 Phan An. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
